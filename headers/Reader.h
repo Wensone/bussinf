4186174c574043298b392f6f2ab5682f94334e00
Direnol <direnol@yandex.ru>
2017-05-01 22:41:23 +0700

    Рур

--- headers/Reader.h
+++ headers/Reader.h
@@ -1,9 +1,23 @@
 #ifndef BASEDATA_READER_H
 #define BASEDATA_READER_H
 
+#include <iostream>
+
+using namespace std;
 
 class Reader {
+private:
 
+    string fio;
+    string address;
+    string email;
+    string book_list; // список взятых книг
+    string time_take; // время взятия
+    string time_using; // срок пользования
+    string book_genre; // жанры
+
+public:
+    friend ostream &operator<<(ostream &os, Reader &rd);
 };
 
 
--- src/Reader.cpp
+++ src/Reader.cpp
@@ -1,1 +1,13 @@
 #include "../headers/Reader.h"
+
+friend ostream &operator<<(ostream &os, Reader &rd)
+{
+    os << rd.fio << endl
+       << rd.address << endl
+       << rd.email << endl
+       << rd.book_list << endl
+       << rd.time_take << endl
+       << rd.time_using << endl
+       << rd.book_genre << endl;
+    return os;
+}
