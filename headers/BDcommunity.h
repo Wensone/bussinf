6163421a83eb68f123e5c5fee32b95cc96372aab
Direnol <Direnol@yandex.ru>
2017-05-03 17:57:38 +0700

    New class

---
+++ headers/BDcommunity.h
@@ -0,0 +1,13 @@
+#ifndef BASEDATA_BDCOMMUNITY_H
+#define BASEDATA_BDCOMMUNITY_H
+
+template <class A, class B>
+class BDcommunity {
+private:
+    A reader;
+    B card;
+public:
+
+};
+
+#endif //BASEDATA_BDCOMMUNITY_H
